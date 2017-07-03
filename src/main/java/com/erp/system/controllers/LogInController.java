f66d02a06c22e6363d38a857710a00323428eac5
klinster <klinster@mail.ru>
2017-07-03 22:21:46 +0300

    added checking for authorization on each url
    добавлен метод проверки на осуществление авторизации и вызов  его в каждом методе с @RM

--- src/main/java/com/erp/system/controllers/LogInController.java
+++ src/main/java/com/erp/system/controllers/LogInController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
@@ -45,7 +46,7 @@ public class LogInController {
     @RequestMapping(value = "/main", method = RequestMethod.POST)
     public String checkUserAuthorization(@ModelAttribute("logPass") @Valid LoginPassword lp,
                                          BindingResult result, Model model
-                                        , HttpServletResponse response) {
+                                        , HttpServletResponse response, HttpServletRequest request) {
         lpValidator.validate(lp, result);
         if(result.hasErrors()){
             return "pages/index";
@@ -53,15 +54,30 @@ public class LogInController {
         String isAdmin = ADMIN.equals(lp.getLogin()) ? TRUE : FALSE;
         response.addCookie(new Cookie(IS_ADMIN, isAdmin));
         model.addAttribute(IS_ADMIN, isAdmin);
+        HttpSession session = request.getSession();
+        session.setAttribute("isLogedIn",TRUE);
         return "pages/main";
     }
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     public String mainPage(Model model, HttpServletRequest request ) {
+        Boolean isLogedIn =isLogedIn(request);
+        if (!isLogedIn){
+            return "redirect:/";
+        }
         Cookie[] cookies = request.getCookies();
         model.addAttribute(IS_ADMIN, getCookieByName(IS_ADMIN, cookies));
         return "pages/main";
     }
-
+    @RequestMapping(value = "/test", method = RequestMethod.GET) // Это будет тестовый метод где будем пробовать новые фичи, чтобы не создавать всегда заново для проверки
+    public String testMethod(HttpServletRequest request){
+        Boolean isLogedIn =isLogedIn(request);
+        if (!isLogedIn){
+            System.out.println("redirect");
+            return "redirect:/";
+        }
+        //каждый наш метод должен начинаться с проверки на осуществление авторизации (пять строк выше), а дальше логика метода
+        return "";
+    }
     //надо придумать как его вынести, чт могли использовать в каждлм контроллере:)
     private String getCookieByName(String cName, Cookie[] cookies) {
         Map<String, Cookie> cookieMap = new HashMap<>();
@@ -70,5 +86,14 @@ public class LogInController {
         }
         Cookie firstRequiredCookie = cookieMap.get(cName);
         return firstRequiredCookie.getValue();
+    }
+    private Boolean isLogedIn(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String str = (String)session.getAttribute("isLogedIn");
+        if (str==null){
+            return false;
+        } else {
+            return true;
+        }
     }
 }
